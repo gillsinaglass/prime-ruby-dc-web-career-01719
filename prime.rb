e63ab78c71836bda18003da7a79b757776d27c32
Ryan Gill <rdgill95@gmail.com>
2018-12-28 00:42:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  array = (2..8).to_a 
+  prime = true
+  if int < 2 
+    prime = false
+  else 
+    array.each do |x|
+      if int % x == 0 
+        prime = false
+      break
+    end
+  end
+end
